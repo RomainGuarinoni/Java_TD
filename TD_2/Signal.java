d35f6ae00f68cb9f1a122b9007c82265802b12a7
RomainGuarinoni <romain.guar91@gmail.com>
2020-10-17 14:43:58 +0200

    Ajout de la fonction correlation

--- TD_2/Signal.java
+++ TD_2/Signal.java
@@ -43,7 +43,7 @@ public class Signal { // coucou alex
             this.tab = new float[frequence * (int) this.duration];
         } else {
             float[] sub_tab = new float[(this.tab).length / frequence];
-            for (int i = 0, j = 0 ; i < (this.tab).length; i = i + frequence, j++) {
+            for (int i = 0, j = 0; i < (this.tab).length; i = i + frequence, j++) {
                 sub_tab[j] = this.tab[i];
             }
             this.tab = sub_tab;
@@ -62,12 +62,11 @@ public class Signal { // coucou alex
 
     public float sum_right(int n, float[] tab, int i) {
         float somme = 0;
-        for(int j=0;j<=n;j++){
-            if(j+i<tab.length){
-                somme+=tab[j+i];
-            }
-            else{
-                somme+=tab[tab.length-1];
+        for (int j = 0; j <= n; j++) {
+            if (j + i < tab.length) {
+                somme += tab[j + i];
+            } else {
+                somme += tab[tab.length - 1];
             }
         }
         return somme;
@@ -75,15 +74,44 @@ public class Signal { // coucou alex
 
     public float sum_left(int n, float[] tab, int i) {
         float somme = 0;
-        for(int j=1;j<=n;j++){
-            if(i-j>=0){
-                somme+=tab[i-j];
-            }
-            else{
-                somme+=tab[0];
+        for (int j = 1; j <= n; j++) {
+            if (i - j >= 0) {
+                somme += tab[i - j];
+            } else {
+                somme += tab[0];
 
             }
         }
         return somme;
+    }
+
+    public double correlation(float[] tab) {
+        double numerateur=0;
+        double denominateur=0;
+        double sub_deno1=0;
+        double sub_deno2=0;
+        if(tab.length!=this.tab.length){ // detection de l'erreur ( j'aurais pu essayer d'utiliser try/catch mais j'ai r compris)
+            System.out.println("Les deux signaux n'ont pas la même taille, le coeff de correlation vaut 0 automatiquement");
+            return 0;
+        }
+        else{
+            for(int i=0;i<tab.length;i++){
+                numerateur+=(tab[i]-sum_signal(tab))*(this.tab[i]-sum_signal(this.tab));
+            }
+            for(int i=0;i<tab.length;i++){
+                sub_deno1+=(tab[i]-sum_signal(tab))*(tab[i]-sum_signal(tab));
+                sub_deno2+=(this.tab[i]-sum_signal(this.tab))*(this.tab[i]-sum_signal(this.tab));
+            }
+            denominateur=(Math.sqrt(sub_deno1))*(Math.sqrt(sub_deno2));
+            return numerateur/denominateur;
+        }
+    }
+
+    public static float sum_signal(float[] tab) {
+        float somme = 0;
+        for (int i = 0; i < tab.length; i++) {
+            somme += tab[i];
+        }
+        return (somme / tab.length);
     }
 }
