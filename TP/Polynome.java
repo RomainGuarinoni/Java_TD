3fd502cfc0501c69f9a8a362d0e81eb439e1558f
Alexandre LAM <alexandrelam@outlook.com>
2020-11-29 20:56:52 +0100

    Question 8

--- TP/Polynome.java
+++ TP/Polynome.java
@@ -108,15 +108,30 @@ public class Polynome {
         return res;
     }
 
+    public Polynome primitivePolynome(Polynome P) {
+        Polynome res = new Polynome(P.degre, P.coefs);
+        for (int i = 0; i < P.degre; i++) {
+            res.coefs[i] = coefs[i] / (i + 1);
+        }
+        res.degre += 1;
+        for (int i = degre; i > 0; i--) {
+            res.coefs[i] = res.coefs[i - 1];
+        }
+        res.coefs[0] = 0;
+        return res;
+    }
+
     public static void main(String[] args) {
         Polynome p = new Polynome();
         System.out.println(p);
-        System.out.println(p.evalPolynome(2));
-        System.out.println(p.evalPolynomeRec(2, p.degre - 1));
         /*
-         * Polynome q = new Polynome(); System.out.println(q); Polynome res =
-         * q.additionPolynome(p, q); System.out.println(res);
-         * System.out.println(p.produitExterne(2, p));
+         * System.out.println(p.evalPolynome(2));
+         * System.out.println(p.evalPolynomeRec(2, p.degre - 1)); Polynome q = new
+         * Polynome(); System.out.println(q); Polynome res = q.additionPolynome(p, q);
+         * System.out.println(res); System.out.println(p.produitExterne(2, p));
+         * System.out.println(p.derivPolynome(p));
          */
-        System.out.println(p.derivPolynome(p));
+        System.out.println(p.primitivePolynome(p));
     }
+
+}
